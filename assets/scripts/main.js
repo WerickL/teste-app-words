0cdeefe7c2fd7483cf05a2997f350afd3b08ed74
Werick.mobile <Werick.mobile>
2022-06-13 20:08:34 -0300

    create list with indice

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,32 @@
-let frase = document.querySelector("#input");
-let palavras = new Array;
+let entrada = document.querySelector("#input");
+var lista = new Map();
+function execução (){
+  newPalavra(entrada.value)
+  entrada.value = ''
+}
+const newPalavra= function (palavra){
+  if (repeated(palavra)){
+    let valueOf = lista.get(palavra);
+    attPalavra(palavra, valueOf)
+  }else{
+     addPalavra(palavra) 
+  }
+}
+function addPalavra(palavra){
+  lista.set(palavra,1);
+}
+function repeated(palavra){
+  return lista.has(palavra)
+}
+function attPalavra(key, value){
+  lista.set(key, ++value)
+}
+
 function showFrase() {
-  palavras.push(frase.value)
-  frase.value = ''
+
 }
 function revealSort(){
-  for(let palavra of palavras){
-    console.log(palavra)
+  for(let palavras of lista){
+    console.log(palavras)
   }
 }
