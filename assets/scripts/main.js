559e7e28917762831c06efd8bb6866a68b82db29
Werick.mobile <Werick.mobile>
2022-06-16 16:18:23 -0300

    Refactor-code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,33 +1,50 @@
+//<Variáveis 
 let entrada = document.querySelector("#input");
 var lista = new Map();
-function execução (){
-  newPalavra(entrada.value)
-  entrada.value = ''
-}
-const newPalavra= function (palavra){
+
+
+//Função principal 
+function newPalavra(palavra){
+  palavra = palavra.trim()
   if (repeated(palavra)){
     let valueOf = lista.get(palavra);
     attPalavra(palavra, valueOf)
   }else{
      addPalavra(palavra) 
   }
+   entrada.value = ''
 }
+
+//Funções componentes 
 function addPalavra(palavra){
   lista.set(palavra,1);
 }
+
+
 function repeated(palavra){
   return lista.has(palavra)
 }
+
+
 function attPalavra(key, value){
   lista.set(key, ++value)
 }
 
 
-function revealSort(){
-  let sortedMap = new Map([...lista.entries()].sort((a,b)=>b[1]-a[1]))
-  const ordenados = Array.from(sortedMap)
+function sortMap(mapInsort){
+  return new Map([...mapInsort.entries()].sort((a,b)=>b[1]-a[1]))
+}
+
+
+ function toArray(map){
+   return Array.from(map)
+ }
  
-  for(let i =0; i < Math.min(10, ordenados.length); ++i){
-    console.log(ordenados[i][0]);
+ 
+function showWords(){
+  let orderedMap = sortMap(lista);
+  let orderedArray = toArray(orderedMap);
+  for(let i = 0; i < Math.min(10, orderedArray.length); ++i){
+    console.log(orderedArray[i][0]);
   }
 }
